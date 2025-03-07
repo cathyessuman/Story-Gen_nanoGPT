82968367b1c5c2028b241a7c667a15e7a0cfbb3c
Catherine Essuman <132572821+cathyessuman@users.noreply.github.com>
2025-03-07 21:25:15 +0200

    Add files via upload

---
+++ train_zulu_prompted.py
@@ -0,0 +1,35 @@
+out_dir = 'out-zulu-prompted'
+# eval stuff
+eval_interval = 250
+eval_iters = 200
+log_interval = 10
+
+wandb_log = True
+wandb_project = 'AIMS-paper'
+wandb_run_name='zulu-prompt'
+
+dataset = 'zulu_prompted'
+batch_size = 64
+block_size = 256
+gradient_accumulation_steps = 1
+
+
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
+init_from = 'scratch'
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
